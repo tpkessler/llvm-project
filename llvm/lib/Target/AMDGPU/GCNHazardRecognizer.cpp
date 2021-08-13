cb9142ce4fc78cf16af0f88b7bcf11f872d3b752
Christudasan Devadasan <Christudasan.Devadasan@amd.com>
2021-08-12 22:54:42 -0400

    [AMDGPU] Skip pseudo MIs in hazard recognizer
    Instructions like WAVE_BARRIER and SI_MASKED_UNREACHABLE
    are only placeholders to prevent certain unwanted
    transformations and will get discarded during assembly
    emission. They should not be counted during nop insertion.
    
    Reviewed By: rampitec
    
    Differential Revision: https://reviews.llvm.org/D108022
    
    Change-Id: I1fbff5049d4945bb8d52853be39cf3ee8c54583c

--- llvm/lib/Target/AMDGPU/GCNHazardRecognizer.cpp
+++ llvm/lib/Target/AMDGPU/GCNHazardRecognizer.cpp
@@ -391,6 +391,10 @@ void GCNHazardRecognizer::AdvanceCycle() {
   }
 
   unsigned NumWaitStates = TII.getNumWaitStates(*CurrCycleInstr);
+  if (!NumWaitStates) {
+    CurrCycleInstr = nullptr;
+    return;
+  }
 
   // Keep track of emitted instructions
   EmittedInstrs.push_front(CurrCycleInstr);
